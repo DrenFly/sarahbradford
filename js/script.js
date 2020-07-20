25e102c65d0794b39d20be06944e776d3908f468
Ailín Draighneán <drenfly@pm.me>
2020-07-20 16:29:01 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menu1 = document.querySelector('.menu-1');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.nav-item');
+
+
+//Set initial State
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if (!showMenu) {
+      menuBtn.classList.add('close');
+      menu.classList.add('show');
+      menuNav.classList.add('show');
+      menu1.classList.add('show');
+      navItems.forEach(item => item.classList.add('show'));
+  
+      // Set Menu State
+      showMenu = true;
+    } else {
+      menuBtn.classList.remove('close');
+      menu.classList.remove('show');
+      menuNav.classList.remove('show');
+      menu1.classList.remove('show');
+      navItems.forEach(item => item.classList.remove('show'));
+  
+      // Set Menu State
+      showMenu = false;
+    }
+  }
